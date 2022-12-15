fecd4ee775cd1541c8859dbc9acc169113eb7ffe
Bhavana <bspalande@gmail.com>
2022-12-15 15:57:13 +0530

    updated varaible values

--- main.tf
+++ main.tf
@@ -16,10 +16,10 @@ terraform {
 
 provider "azurerm" {
     version         =   "~> 2.0"
-    client_id       =   'edef04e2-c2cb-4db9-b50c-245c86f851fb'
-    client_secret   =   'GJp8Q~4ufHwvmKSNjyTi.wC4XUy7njnsbEZAbang'
-    subscription_id =   '0444f5bd-f158-45de-9a56-190ccc7ee8c3'
-    tenant_id       =   '069ca8c2-34f8-4786-940a-96d33e8cf1f3'
+    client_id       =   "edef04e2-c2cb-4db9-b50c-245c86f851fb"
+    client_secret   =   "GJp8Q~4ufHwvmKSNjyTi.wC4XUy7njnsbEZAbang"
+    subscription_id =   "0444f5bd-f158-45de-9a56-190ccc7ee8c3"
+    tenant_id       =   "069ca8c2-34f8-4786-940a-96d33e8cf1f3"
     
     features {}
 }
@@ -29,9 +29,9 @@ provider "azurerm" {
 
 provider "azuread" {
     version         =   ">= 0.11"
-    client_id       =   'edef04e2-c2cb-4db9-b50c-245c86f851fb'
-    client_secret   =   'GJp8Q~4ufHwvmKSNjyTi.wC4XUy7njnsbEZAbang'
-    tenant_id       =   '069ca8c2-34f8-4786-940a-96d33e8cf1f3'
+    client_id       =   "edef04e2-c2cb-4db9-b50c-245c86f851fb"
+    client_secret   =   "GJp8Q~4ufHwvmKSNjyTi.wC4XUy7njnsbEZAbang"
+    tenant_id       =   "069ca8c2-34f8-4786-940a-96d33e8cf1f3"
     alias           =   "ad"
 }
 
